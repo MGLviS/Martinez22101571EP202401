0ce30f8f59720f134857f23b8f573010f89f5169
LuisM <118645322+MGLviS@users.noreply.github.com>
2024-05-19 12:04:30 -0500

    Menu Ok

--- app/src/main/java/com/example/martinez22101571ep202401/LoginActivity.kt
+++ app/src/main/java/com/example/martinez22101571ep202401/LoginActivity.kt
@@ -31,7 +31,7 @@ class LoginActivity : AppCompatActivity() {
                 return@setOnClickListener
             } else{
                 if(etUsuario.text.toString() == "admin" && etPassword.text.toString() == "admin123"){
-                    val intent = Intent(this, MainActivity::class.java)
+                    val intent = Intent(this, MenuActivity::class.java)
                     startActivity(intent)
                 } else{
                     Toast.makeText(this,"Usuario o contraseña incorrectos",Toast.LENGTH_LONG).show()
